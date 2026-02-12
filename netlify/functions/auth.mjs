333f847e56264d87762fe2db4faa753cae7efa68
Travis Gray <travis@postlaboreconomics.com>
2026-02-12 23:25:06 +0000

    Fix: import getCurrentUser in auth.mjs for profile update

--- netlify/functions/auth.mjs
+++ netlify/functions/auth.mjs
@@ -1,4 +1,4 @@
-import { getDb, hashToken, logActivity, jsonResponse } from './lib/db.mjs';
+import { getDb, hashToken, getCurrentUser, logActivity, jsonResponse } from './lib/db.mjs';
 import bcrypt from 'bcryptjs';
 import { v4 as uuidv4 } from 'uuid';
 
